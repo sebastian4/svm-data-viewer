16ce6f7b95a2b0269807c1e81d5e785ddaf74d13
sebastian <sebastianmac@Sebastians-MBP.home>
2014-12-10 22:08:00 -0500

    added more views

---
+++ app/scripts/ang-seeds-1/app-11/view4/view4.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.view4', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view4', {
+    templateUrl: 'view4/view4.html',
+    controller: 'View4Ctrl'
+  });
+}])
+
+.controller('View4Ctrl', [function() {
+
+}]);
