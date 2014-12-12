0c23a14f03eccfcf7ef628c13970a1cc008ad009
sebastian <sebastianmac@Sebastians-MBP.home>
2014-12-12 09:55:21 -0500

    fixed with the correct item names

--- app/scripts/angulars-1/angular-demo-2-1/todo.js
+++ app/scripts/angulars-1/angular-demo-2-1/todo.js
@@ -1,8 +1,8 @@
 function TodoCtrl($scope) {
 
   $scope.todos = [
-    { title:'learn angular', completed:true },
-    { title:'build an angular app', completed:false }
+    { id: 101, userId: 1, title:'learn angular', completed:true },
+    { id: 102, userId: 1, title:'build an angular app', completed:false }
   ];
 
   ////
@@ -22,18 +22,43 @@ function TodoCtrl($scope) {
   ////
 
   $scope.addTodo = function() {
-    $scope.todos.push({ title:$scope.todoText, completed:false });
+    console.log("addTodo");
+    var newTodo = { 
+      id: $scope.getRandomNumber(),
+      userId: 1,
+      title: $scope.todoText, 
+      completed: false 
+    };
+    console.log(newTodo);
+    $scope.todos.push(newTodo);
     $scope.todoText = '';
   };
 
   ////
 
   $scope.archiveTodos = function() {
+    console.log("archiveTodos");
     var oldTodos = $scope.todos;
     $scope.todos = [];
     angular.forEach(oldTodos, function(todo) {
       if (!todo.completed) $scope.todos.push(todo);
     });
+  };
+
+  $scope.getRandomNumber = function() {
+    var timeInMs = Date.now();
+    var randomNum = (timeInMs % 1000000000);
+    return randomNum;
+  };
+
+  $scope.debug = function() {
+    console.log("debug");
+
+    var timeInMs = Date.now();
+    console.log(timeInMs);
+    var randomNum = (timeInMs % 1000000000);
+    console.log(randomNum);
+
   };
 
 }
