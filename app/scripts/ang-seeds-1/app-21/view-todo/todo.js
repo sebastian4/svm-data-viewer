68397c4b815573c775a35573c43318700c6e45c3
sebastian <sebastianmac@Sebastians-MBP.home>
2014-12-12 10:28:22 -0500

    made correct changes for todos

--- app/scripts/ang-seeds-1/app-21/view-todo/todo.js
+++ app/scripts/ang-seeds-1/app-21/view-todo/todo.js
@@ -9,6 +9,67 @@ angular.module('myApp.todo', ['ngRoute'])
   });
 }])
 
-.controller('TodoCtrl', [function() {
+.controller('TodoCtrl', ['$scope', function($scope) {
+
+  $scope.todos = [
+    { id: 101, userId: 1, title:'learn angular', completed:true },
+    { id: 102, userId: 1, title:'build an angular app', completed:false }
+  ];
+
+  ////
+ 
+  $scope.todosRemaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.completed ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.todosSize = function() {
+    return $scope.todos.length;
+  }
+ 
+  ////
+
+  $scope.addTodo = function() {
+    console.log("addTodo");
+    var newTodo = { 
+      id: $scope.getRandomNumber(),
+      userId: 1,
+      title: $scope.todoText, 
+      completed: false 
+    };
+    console.log(newTodo);
+    $scope.todos.push(newTodo);
+    $scope.todoText = '';
+  };
+
+  ////
+
+  $scope.archiveTodos = function() {
+    console.log("archiveTodos");
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.completed) $scope.todos.push(todo);
+    });
+  };
+
+  $scope.getRandomNumber = function() {
+    var timeInMs = Date.now();
+    var randomNum = (timeInMs % 1000000000);
+    return randomNum;
+  };
+
+  $scope.debug = function() {
+    console.log("debug");
+
+    var timeInMs = Date.now();
+    console.log(timeInMs);
+    var randomNum = (timeInMs % 1000000000);
+    console.log(randomNum);
+
+  };
 
 }]);
