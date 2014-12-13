f7ea7c67b1dfcc4a526d6efc287870d303c325c3
sebastian <sebastianmac@Sebastians-MBP.home>
2014-12-13 18:47:09 -0500

    finished the last view

--- app/scripts/ang-seeds-1/app-11/view4/view4.js
+++ app/scripts/ang-seeds-1/app-11/view4/view4.js
@@ -14,9 +14,53 @@ angular.module('myApp.view4', ['ngRoute'])
 	console.log("view4 controller again");
 
 	$scope.loans = [];
+	$scope.currentLoan = null;
+
+	var container = document.getElementById('mytimeline');
+	var options = {};
+	var timeline = null;
 
 	if (undefined!==$rootScope.loans) {
+		console.log("rootScope loans was undefined");
 		$scope.loans = $rootScope.loans;
+		$scope.currentLoan = null;
+
+		var data = new vis.DataSet([]);
+		
+		timeline = new vis.Timeline(container, data, options);
+		if (null!==timeline) {
+			console.log("timeline destroy");
+			timeline.destroy();
+			timeline = null;
+		}
+	}
+
+	////
+
+	$scope.updateLoan = function() {
+	   	console.log("updateLoan");
+
+	    var timePoints = [];
+
+	    var paymentsLength = $scope.currentLoan.payments.length;
+		for (var i = 0; i < paymentsLength; i++) {
+			var dueDate = moment($scope.currentLoan.payments[i].due_date).format("YYYY-MM-DD");
+			var timePoint = {
+				id: i+1,
+				content: Math.trunc($scope.currentLoan.payments[i].amount),
+				start: dueDate
+			};
+			timePoints.push(timePoint);
+		}
+
+		var data = new vis.DataSet(timePoints);
+
+		if (null!==timeline) {
+			console.log("timeline destroy");
+			timeline.destroy();
+		}
+	    timeline = new vis.Timeline(container, data, options);
+
 	}
 
 	////
@@ -25,14 +69,18 @@ angular.module('myApp.view4', ['ngRoute'])
 		return $scope.loans.length;
 	}
 
+	$scope.paymentsSize = function() {
+		if (null !== $scope.currentLoan) {
+			return $scope.currentLoan.payments.length;
+		}
+		return 0;
+	}
 
 	////
 
 	$scope.debug = function() {
 	    console.log("debug");
-
 	    console.log($scope.loans.length);
-
 	};
 
 }]);
